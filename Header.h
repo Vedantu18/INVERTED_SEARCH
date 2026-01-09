a4548b98bb888221d62f7f8073a7b7ce974753dd
Vedantu18 <vedantshivsharan14@gmail.com>
2026-01-09 12:50:30 +0530

    Add initial header file for inverted search implementation

---
+++ Header.h
@@ -0,0 +1,18 @@
+#ifndef INVERTED_SEARCH_H
+#define INVERTED_SEARCH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define FAILURE -1
+#define SIZE 26
+
+typedef struct main_node
+{
+    
+};
+
+#endif
