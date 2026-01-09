17be020d915ba921b0d5f55c0a6a1eec712350ab
Vedantu18 <vedantshivsharan14@gmail.com>
2026-01-09 13:28:23 +0530

    Refactor Header.h to define sub_node structure and update main_node structure

--- Header.h
+++ Header.h
@@ -10,9 +10,21 @@
 #define FAILURE -1
 #define SIZE 26
 
+/*structure for sub node*/
+typedef struct sub_node
+{
+    int word_count;
+    char file_name[50];
+    struct sub_node *link;
+}sub_node;
+
+/*structure for sub main node*/
 typedef struct main_node
 {
-    
-};
+    int file_count;
+    char word[50];
+    struct sub_node *sub_link;
+    struct main_node*main_link;
+}main_node;
 
 #endif
