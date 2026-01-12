cc77fcbed92322c903276981e53be4957e99482c
Vedantu18 <vedantshivsharan14@gmail.com>
2026-01-12 23:55:04 +0530

    Fix validation check in main function and enhance print_list for better output

--- main.c
+++ main.c
@@ -2,8 +2,31 @@
 int main(int argc, char *argv[])
 {
     valid_file *head = NULL;
-    if(validation_of_file(&head, argc, argv))
+
+    if (validation_of_file(&head, argc, argv) == FAILURE)
     {
-        return SUCCESS;
+        return 1;
+    }
+
+    print_list(head);
+    return 0;
+}
+
+
+void print_list(valid_file *head)
+{
+	if (head == NULL)
+	{
+		printf("INFO : List is empty\n");
+	}
+    else
+    {
+	    while (head)		
+	    {
+		    printf("%s -> ", head -> Files);
+		    head = head -> link;
+	    }
+
+	    printf("NULL\n");
     }
 }
