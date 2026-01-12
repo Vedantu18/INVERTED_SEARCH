93cb4955dddc0151ef0cc7907b7fe8cb10503a31
Vedantu18 <vedantshivsharan14@gmail.com>
2026-01-12 23:54:49 +0530

    Refactor validation_of_file function to streamline file existence check and correct duplicate file logic

--- Validation.c
+++ Validation.c
@@ -8,19 +8,16 @@ int validation_of_file(valid_file **head, int argc, char *argv[])
             continue;
 
         /*File should exists or not*/
-        FILE *fp = fopen(argv[i], "r");
-        if (fp == NULL)
-        {
+        if (is_exists_or_empty(argv[i]) == FAILURE)
             continue;
-        }
-        fclose(fp);
 
         /*check the file is duplicate or not*/
-        if (is_duplicate_file(*head, argv[i]) == SUCCESS)
+        if (is_duplicate_file(*head, argv[i]) == FAILURE)
             continue;
 
-            
+        create_list(head, argv[i]);
     }
+    return SUCCESS;
 }
 
 int is_duplicate_file(valid_file *head, char *filename)
@@ -28,11 +25,53 @@ int is_duplicate_file(valid_file *head, char *filename)
     valid_file *temp = head;
     while (temp != NULL)
     {
-        if (strcmp(temp->file_create, filename) == 0)
+        if (strcmp(temp->Files, filename) == 0)
         {
-            return SUCCESS;
+            return FAILURE; // File is Duplicate
         }
         temp = temp->link;
     }
-    return FAILURE;
+    return SUCCESS; // File Not Duplicate
+}
+
+int is_exists_or_empty(char *filename)
+{
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("ERROR : File not opened\n");
+        return FAILURE;
+    }
+    fseek(fp, 0, SEEK_END);
+    if (ftell(fp) == 0)
+    {
+        fclose(fp);
+        return FAILURE; // File is Empty
+    }
+    fclose(fp);
+    return SUCCESS; // File exists and it is not empty
+}
+
+int create_list(valid_file **head, char *filename)
+{
+    valid_file *new = malloc(sizeof(valid_file));
+    if(new == NULL)
+    {
+        return FAILURE;
+    }
+    strcpy(new->Files, filename);
+    new->link = NULL;
+    
+    if(*head == NULL)
+    {
+        *head = new;
+        return SUCCESS;
+    }
+    valid_file *temp = *head;
+    while(temp->link != NULL)
+    {
+        temp = temp->link;
+    }
+    temp->link = new;
+    return SUCCESS;
 }
